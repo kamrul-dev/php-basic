e4a218c0e6816858ad6e3af898597ff953edd806
Kamrul Hasan <kamrulsdev@gmail.com>
2024-06-13 03:50:50 +0600

    php with html code

--- 2_php-syntax.php
+++ 2_php-syntax.php
@@ -3,3 +3,23 @@
 
 
 // php script ends with this symbol ?>
+
+
+<!-- In PHP, keywords (e.g. if, else, while, echo, etc.), classes, functions, and user-defined functions are not case-sensitive. -->
+
+ <!-- php with html code -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "hello world";
+        ECHO "hello world";
+        eCho "Hello world";
+    ?>
+</body>
+</html>
